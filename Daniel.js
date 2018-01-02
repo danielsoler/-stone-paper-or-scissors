c1e9f8bd3aea0411f4c25dec230ef53a60520987
Daniel <cdsg.6.20.13@gmail.com>
2018-01-02 13:42:09 -0500

    terminado

--- Daniel.js
+++ Daniel.js
@@ -17,22 +17,21 @@ function play() {
 	}
 
 	if (error_counter >= max_error) {
-		return;
 		alert('Excedio la cantidad de errores permitidos, bestia');
+		return;
 	}
 
 	jugador2 = prompt("hola jugador2, como es tu nombre?")
 	seleccionj2 = prompt(jugador2 + " " + "juega")
-
-
+	
 	while(seleccionj2 != 'piedra' && seleccionj2 != 'papel' && seleccionj2 != 'tijera' && error_counterj2 < max_error) {
 	    seleccionj2 = prompt('ingrese una opción pero que sirva');
 	}
 
 
 	if (error_counterj2 >= max_error) {
-		return;
 		alert('Excedio la cantidad de errores permitidos, bestia');
+		return;
 	}
 
 	if (seleccionj1 == piedra) {
